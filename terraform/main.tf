2970d9f41a6a75e156eb799f8945bade48dd8995
Jaco Pretorius <me@jacopretorius.net>
2026-08-25 14:55:43 -0500

    Remove redundant S3 encryption configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,16 +34,6 @@ resource "aws_s3_bucket_versioning" "review_state" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "review_state" {
-  bucket = aws_s3_bucket.review_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "review_state" {
   bucket = aws_s3_bucket.review_state.id
 
